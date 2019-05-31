a11669bc7fe6723f6bdbd6ae76f5146cd8922812
Luis David Morales <luisdavidmorales95@gmail.com>
2019-05-31 14:12:34 -0500

    primer formulario

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,3 +1,5 @@
+const path = require('path');
+const { randomNumber } = require('../helpers/libs');
 //Un controlador es un objeto con funciones
 const ctrl ={};
 
@@ -6,7 +8,13 @@ ctrl.index = (req, res)=>{
 };
 
 ctrl.create = (req, res)=>{
-    
+    const imgUrl= randomNumber();
+    console.log(imgUrl);
+    const imageTempPath = req.file.path;
+    const ext = path.extname(req.file.originalname).toLowerCase(); //para obtener la extension de la imagen
+    const targetPath = path.resolve('src/public/upload/${imgUrl}${ext}');
+
+    res.send('Works!');
 };
 
 ctrl.like = (req, res)=>{
